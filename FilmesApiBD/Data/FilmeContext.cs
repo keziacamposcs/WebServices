001fe75bc1ac6189a408915df5a6ca6ea32c40df
Kezia Campos <kezia.campos@icloud.com>
2023-05-23 08:27:09 -0300

    update

---
+++ FilmesApiBD/Data/FilmeContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FilmesApiBD.Models;
+
+namespace FilmesApiBD.Data
+{
+    public class FilmeContext : DbContext
+    {
+        public FilmeContext(DbContextOptions<FilmeContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Filme> Filmes { get; set; }
+    }
+    
+}
---
+++ FilmesApiBD/Models/Filme.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApiBD.Models
+{
+    public class Filme
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Titulo { get; set; }
+        [Required]
+        public string Genero { get; set; }
+        [Required]
+        public int Duracao { get; set; }
+    }
+}
